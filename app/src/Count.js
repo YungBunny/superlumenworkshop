a309434e0a3548de64126712f364ffb7e925b4bd
Chanel Fu <chanelfu@Chanels-MacBook-Pro-2.local>
2018-05-17 20:42:11 +0200

    add an action dispatcher

--- app/src/Count.js
+++ app/src/Count.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { inc } from './actions';
+
 const Count = props => {
     const {theCounterIsAt } = props;
     return <div>
     <h2>Count FTW</h2>
     <p>The counter is at {theCounterIsAt}</p>
+    <p>
+        <button>
+        +1
+        </button>
+    </p>
 </div>
 };
 
@@ -15,4 +22,14 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(Count);
+const mapDispatchToProps = dispatch => {
+    return {
+        putTheNumberUp: () => dispatch(inc())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Count);
+
+// create actions with action creators
+// dispatch actions
+// reducers take the current state and the action and calculate the new state
