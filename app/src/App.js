a4e8bb139f1c530c826042bcaa0788357085dbd8
Chanel Fu <chanelfu@Chanels-MacBook-Pro-2.local>
2018-05-17 19:59:25 +0200

    separate reducer file

--- app/src/App.js
+++ app/src/App.js
@@ -4,30 +4,25 @@ import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const empty = {
-  counter: 0,
-  menu: {
-    isOpen: false
-  }
-};
-
-const reducer = (state = empty, action) => state;
+import reducer from "./reducer"
 
 const store = createStore(reducer);
+
+
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </Provider>
     );
   }
