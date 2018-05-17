0c3673a97f5ae9109e910f9707ba18a2f25fe2b4
Chanel Fu <chanelfu@Chanels-MacBook-Pro-2.local>
2018-05-17 19:56:04 +0200

    added provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const empty = {
+  counter: 0,
+  menu: {
+    isOpen: false
+  }
+};
+
+const reducer = (state = empty, action) => state;
+
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +28,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </Provider>
     );
   }
 }
